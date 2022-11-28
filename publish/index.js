444fd210895818d7579a99756b86dde245a63ec8
ieifi <leochen36@live.com>
2022-11-28 20:34:34 +0800

    add

--- publish/index.js
+++ publish/index.js
@@ -5,7 +5,8 @@ var __webpack_exports__ = {};
     let prefix = hostname == "127.0.0.1" ? "" : `/${hostname.replace(/[\.]/g, "-")}`;
     if (!prefix)
         return;
-    location.href = location.origin + prefix + location.pathname + location.search + location.hash;
+    let protocol = "http:" == location.protocol ? "https:" : location.protocol;
+    location.href = protocol + "//" + location.host + +prefix + location.pathname + location.search + location.hash;
 })();
 
 module.exports = __webpack_exports__;
--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,10 @@
    let hostname = location.hostname;
    let prefix = hostname == "127.0.0.1" ? "" : `/${hostname.replace(/[\.]/g, "-")}`;
    if (!prefix) return;
-   location.href = location.origin + prefix + location.pathname + location.search + location.hash;
+   /*    if ("http:" == location.protocol) {
+      location.href = "https://" + location.host + location.pathname + location.search + location.hash;
+      return;
+   } */
+   let protocol = "http:" == location.protocol ? "https:" : location.protocol;
+   location.href = protocol + "//" + location.host + +prefix + location.pathname + location.search + location.hash;
 })();

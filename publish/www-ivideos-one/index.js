1ffa770d5daa5b3fa7fbca2a7b1eea362f62a19f
ieifi <leochen36@live.com>
2022-11-29 13:26:35 +0800

    add

--- publish/www-ivideos-one/index.js
+++ publish/www-ivideos-one/index.js
@@ -3,7 +3,5 @@
    let lang = langs.has(navigator.language) ? navigator.language : "en";
    lang = lang + "/";
    let nurl = location.origin + location.pathname + lang + location.search + location.hash;
-   setTimeout(() => {
-      location.href = nurl;
-   }, 1000);
+   location.href = nurl;
 })();

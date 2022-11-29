9af9d8e771ffe6bc55a30d93685079957996674c
ieifi <leochen36@live.com>
2022-11-29 12:10:47 +0800

    add

--- tslint.js
+++ tslint.js
@@ -1,5 +1,5 @@
 module.exports = {
   defaultSeverity: "none",
-  extend: ['tslint:disable'],
+  //extend: ['tslint:disable'],
   //extend: ["tslint-config-prettier"], // 继承 tslint-config-prettier
 };

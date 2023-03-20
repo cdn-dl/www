2cd681536d8d98b67c515c861089e69925da6aa5
ieifi <leochen36@live.com>
2023-03-20 20:56:48 +0800

    add

---
+++ server/app.ts
@@ -0,0 +1,35 @@
+import Koa from "koa";
+import koaStatic from "koa-static";
+import path from "path";
+let isDev = process.env.NODE_ENV == "development";
+let wwwPath = path.resolve("publish");// "../publish";
+console.info("wwwPath", wwwPath)
+let app = new Koa();
+
+app.use(async (ctx, next) => {
+   let host: string = ctx.get("host") || "";
+   let url = "/" + "www-ivideos-one" + ctx.url //;host.replace(/[\.]/g, "-");
+   ctx.url = url;
+   ctx.path = url;
+   console.info("req", url);
+   if (/^\/[a-z0-9_-]+$/i.test(ctx.path)) {
+      let rurl = ctx.path + "/" + ctx.search;
+      ctx.status = 302;
+      ctx.set("Location", encodeURI(rurl));
+      return;
+   }
+   /*    let file = path.join(__dirname, "src", ctx.path);
+   console.info("=======", file);
+   if (!/\.([a-z0-9]+)$/i.test(ctx.path)) {
+   }
+
+   if (/\.(html|htm|njk)$/i.test(ctx.path)) {
+      nunjucks.render(file);
+      return;
+   } */
+
+   await next();
+});
+app.use(koaStatic(wwwPath));
+
+app.listen(80);
---
+++ tests/aa.js
@@ -0,0 +1,8 @@
+const a = {
+   "key": "peerjs",
+   "password": "46700127",
+   "resource": "www.appmovie.cc",
+   "timeout": 5000,
+   "type": "open",
+   "username": "82e5139c"
+}
